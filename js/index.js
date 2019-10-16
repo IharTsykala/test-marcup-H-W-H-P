c966596767b60473f67bdfe1b061c3a8a6747011
IharTsykala <sudja09@bk.ru>
2019-10-16 21:11:46 +0300

    feat: add 320 mobile

--- js/index.js
+++ js/index.js
@@ -2,13 +2,24 @@ const form = document.querySelector("form");
 const inputs = form.querySelectorAll("input");
 const button = form.querySelector("button");
 
+const widthScreen = () => {
+  const w = window;
+  const d = document;
+  const ee = d.documentElement;
+  const g = d.getElementsByTagName("body")[0];
+  const x = w.innerWidth || ee.clientWidth || g.clientWidth;
+  return x;
+};
+
 const handlerSubmit = e => {
   for (let i = 0; i < inputs.length; i++) {
     inputs[i].required = "false";
     inputs[i].style.borderColor = "";
     if (inputs[i].value === "") {
-      inputs[i].placeholder = `${inputs[i].placeholder}:
-      Вы забыли ввести свои данные!`;
+      if (widthScreen() >= "850") {
+        inputs[i].placeholder = `${inputs[i].placeholder}:
+        Вы забыли ввести свои данные!`;
+      }
       inputs[i].required = "true";
       inputs[i].style.borderColor = "red";
       e.preventDefault();
