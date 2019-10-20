452f84a93120018aadbbfb59345a98e13526f5e3
IharTsykala <sudja09@bk.ru>
2019-10-20 19:45:13 +0300

    feat: add random animation with js

--- js/index.js
+++ js/index.js
@@ -33,3 +33,93 @@ const handlerSubmit = e => {
 button.addEventListener("click", e => {
   handlerSubmit(e);
 });
+
+// copypasta code from http://jsfiddle.net/2TUFF/
+window.onload = function() {
+  $(document).ready(function() {
+    animateDiv();
+  });
+
+  function makeNewPosition($container) {
+    $container = $container || $(window);
+    var h = $container.height() - 50;
+    var w = $container.width() - 50;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+  }
+
+  function animateDiv() {
+    var $appleFooterBody = $(".apple-footer-body");
+    var newqAppleFooterBody = makeNewPosition($appleFooterBody.parent());
+    var oldqAppleFooterBody = $appleFooterBody.offset();
+    var speed = calcSpeed(
+      [oldqAppleFooterBody.top, oldqAppleFooterBody.left],
+      newqAppleFooterBody
+    );
+
+    var $berryBody = $(".berry-body");
+    var newqBerryBody = makeNewPosition($berryBody.parent());
+    var oldqBerryBody = $berryBody.offset();
+    var speed = calcSpeed(
+      [oldqBerryBody.top, oldqBerryBody.left],
+      newqBerryBody
+    );
+
+    var $mainVoteStrawberry = $(".main__vote__strawberry");
+    var newqMainVoteStrawberry = makeNewPosition($mainVoteStrawberry.parent());
+    var oldqMainVoteStrawberry = $mainVoteStrawberry.offset();
+    var speed = calcSpeed(
+      [oldqMainVoteStrawberry.top, oldqMainVoteStrawberry.left],
+      newqMainVoteStrawberry
+    );
+
+    $(".apple-footer-body").animate(
+      {
+        top: newqAppleFooterBody[0],
+        left: newqAppleFooterBody[1]
+      },
+      speed,
+      function() {
+        animateDiv();
+      }
+    );
+
+    $(".berry-body").animate(
+      {
+        top: newqBerryBody[0],
+        left: newqBerryBody[1]
+      },
+      speed,
+      function() {
+        animateDiv();
+      }
+    );
+
+    $(".main__vote__strawberry").animate(
+      {
+        top: newqMainVoteStrawberry[0],
+        left: newqMainVoteStrawberry[1]
+      },
+      speed,
+      function() {
+        animateDiv();
+      }
+    );
+  }
+
+  function calcSpeed(prev, next) {
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+
+    var greatest = x > y ? x : y;
+
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest / speedModifier);
+
+    return speed;
+  }
+};
