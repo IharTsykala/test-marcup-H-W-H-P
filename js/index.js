417f9dea7dc39b159e61a7ee30ce9d72b2e7004a
Ihar Tsykala <Sudja09@bk.ru>
2019-12-03 20:56:42 +0300

    fix:fix bounds width wrapper 1920

--- js/index.js
+++ js/index.js
@@ -1,80 +1,80 @@
-const form = document.querySelector("form");
-const inputs = form.querySelectorAll("input");
-const button = form.querySelector("button");
+const form = document.querySelector("form")
+const inputs = form.querySelectorAll("input")
+const button = form.querySelector("button")
 
 const widthScreen = () => {
-  const w = window;
-  const d = document;
-  const ee = d.documentElement;
-  const g = d.getElementsByTagName("body")[0];
-  const x = w.innerWidth || ee.clientWidth || g.clientWidth;
-  return x;
-};
+  const w = window
+  const d = document
+  const ee = d.documentElement
+  const g = d.getElementsByTagName("body")[0]
+  const x = w.innerWidth || ee.clientWidth || g.clientWidth
+  return x
+}
 
 const handlerSubmit = e => {
   for (let i = 0; i < inputs.length; i++) {
-    inputs[i].required = "false";
-    inputs[i].style.borderColor = "";
+    inputs[i].required = "false"
+    inputs[i].style.borderColor = ""
     if (inputs[i].value === "") {
       if (
         widthScreen() >= "850" &&
         inputs[i].placeholder.indexOf("Вы забыли ввести свои данные!") === -1
       ) {
         inputs[i].placeholder = `${inputs[i].placeholder}:
-        Вы забыли ввести свои данные!`;
+        Вы забыли ввести свои данные!`
       }
-      inputs[i].required = "true";
-      inputs[i].style.borderColor = "red";
-      e.preventDefault();
+      inputs[i].required = "true"
+      inputs[i].style.borderColor = "red"
+      e.preventDefault()
     }
   }
-};
+}
 
 button.addEventListener("click", e => {
-  handlerSubmit(e);
-});
+  handlerSubmit(e)
+})
 
 // copypasta code from http://jsfiddle.net/2TUFF/
 window.onload = function() {
   $(document).ready(function() {
-    animateDiv();
-  });
+    animateDiv()
+  })
 
   function makeNewPosition($container) {
-    $container = $container || $(window);
-    var h = $container.height() - 50;
-    var w = $container.width() - 50;
+    $container = $container || $(window)
+    var h = $container.height() - 50
+    var w = $container.width() - 50
 
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
+    var nh = Math.floor(Math.random() * h)
+    var nw = Math.floor(Math.random() * w)
 
-    return [nh, nw];
+    return [nh, nw]
   }
 
   function animateDiv() {
-    var $appleFooterBody = $(".apple-footer-body");
-    var newqAppleFooterBody = makeNewPosition($appleFooterBody.parent());
-    var oldqAppleFooterBody = $appleFooterBody.offset();
+    var $appleFooterBody = $(".apple-footer-body")
+    var newqAppleFooterBody = makeNewPosition($appleFooterBody.parent())
+    var oldqAppleFooterBody = $appleFooterBody.offset()
     var speed = calcSpeed(
       [oldqAppleFooterBody.top, oldqAppleFooterBody.left],
       newqAppleFooterBody
-    );
+    )
 
-    var $berryBody = $(".berry-body");
-    var newqBerryBody = makeNewPosition($berryBody.parent());
-    var oldqBerryBody = $berryBody.offset();
+    var $berryBody = $(".berry-body")
+    var newqBerryBody = makeNewPosition($berryBody.parent())
+    var oldqBerryBody = $berryBody.offset()
     var speed = calcSpeed(
       [oldqBerryBody.top, oldqBerryBody.left],
       newqBerryBody
-    );
+    )
 
-    var $mainVoteStrawberry = $(".main__vote__strawberry");
-    var newqMainVoteStrawberry = makeNewPosition($mainVoteStrawberry.parent());
-    var oldqMainVoteStrawberry = $mainVoteStrawberry.offset();
+    var $mainVoteStrawberry = $(".main__vote__strawberry")
+    var newqMainVoteStrawberry = makeNewPosition($mainVoteStrawberry.parent())
+    var oldqMainVoteStrawberry = $mainVoteStrawberry.offset()
     var speed = calcSpeed(
       [oldqMainVoteStrawberry.top, oldqMainVoteStrawberry.left],
       newqMainVoteStrawberry
-    );
+    )
 
     $(".apple-footer-body").animate(
       {
@@ -83,9 +83,9 @@ window.onload = function() {
       },
       speed,
       function() {
-        animateDiv();
+        animateDiv()
       }
-    );
+    )
 
     $(".berry-body").animate(
       {
@@ -94,9 +94,9 @@ window.onload = function() {
       },
       speed,
       function() {
-        animateDiv();
+        animateDiv()
       }
-    );
+    )
 
     $(".main__vote__strawberry").animate(
       {
@@ -105,21 +105,21 @@ window.onload = function() {
       },
       speed,
       function() {
-        animateDiv();
+        animateDiv()
       }
-    );
+    )
   }
 
   function calcSpeed(prev, next) {
-    var x = Math.abs(prev[1] - next[1]);
-    var y = Math.abs(prev[0] - next[0]);
+    var x = Math.abs(prev[1] - next[1])
+    var y = Math.abs(prev[0] - next[0])
 
-    var greatest = x > y ? x : y;
+    var greatest = x > y ? x : y
 
-    var speedModifier = 0.1;
+    var speedModifier = 0.1
 
-    var speed = Math.ceil(greatest / speedModifier);
+    var speed = Math.ceil(greatest / speedModifier)
 
-    return speed;
+    return speed
   }
-};
+}
